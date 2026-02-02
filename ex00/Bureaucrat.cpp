6327a8004b147f5be8d11d31f809eb2270a52411
Amal Rashid <amrashid@c2r1s7.42amman.com>
2026-02-02 13:23:29 +0300

    ex00

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,57 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void) {}
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name) {
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+	_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name) {
+	_grade = other._grade;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other){
+	if (this != &other) {
+		_grade = other._grade;
+	}
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat(void) {}
+
+std::string Bureaucrat::getName(void) const{
+	return (_name);
+}
+
+int Bureaucrat::getGrade(void) const{
+	return (_grade);
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw() {
+	return ("Grade is too high");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw() {
+	return ("Grade is too low");
+}
+
+void Bureaucrat::IncrementGrade() {
+	_grade--;
+	if (_grade < 1)
+		throw GradeTooHighException();
+}
+
+void Bureaucrat::DecrementGrade() {
+	_grade++;
+	if (_grade > 150)
+		throw GradeTooLowException();
+}
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat bureaucrat) {
+	os << bureaucrat.getName() << ", bureacrat grade " << bureaucrat.getGrade() << ".";
+	return (os);
+}
---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,38 @@
+#ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <iostream>
+#include <exception>
+
+class Bureaucrat{
+
+	const std::string	_name;
+	int					_grade;
+
+public:
+	Bureaucrat(void);
+	Bureaucrat(const std::string name, int grade);
+	Bureaucrat(const Bureaucrat& other);
+	Bureaucrat& operator=(const Bureaucrat& other);
+	~Bureaucrat(void);
+
+	std::string getName() const;
+	int getGrade() const;
+
+	class GradeTooHighException : public std::exception {
+	public:
+		const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception {
+	public:
+		const char* what() const throw();
+	};
+
+	void IncrementGrade();
+	void DecrementGrade();
+};
+
+std::ostream &operator<<(std::ostream &os, Bureaucrat bureaucrat);
+
+#endif
